e70a270403d9b9ab4f18536088c4d9e3300778d0
4y0 <4y0d3jii@gmail.com>
2017-01-16 22:01:00 +0100

    lOGIN ENDPOINT ROUTE

--- routes/v1/handlers/login.js
+++ routes/v1/handlers/login.js
@@ -1,3 +1,16 @@
+var loginService = require('../../../services/login');
+var utils = require('../../../utils');
 module.exports = function (req, res, next){
-	res.json({a:1});
+		
+		loginService(req.body).then( (token)=>{
+
+			utils.jsonS(res, token, "Login successful");
+
+		})
+		.catch((err) => {
+
+			utils.jsonF(res, null, err.message);
+
+		})
+
 }
